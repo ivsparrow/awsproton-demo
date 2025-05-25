f4b1259e45bb859194ac15ffc9e1caa6248a5c22
Ihor Vorobyenko <ihor.vorobyenko@blackbit.de>
2025-05-25 18:57:12 +0300

    add new files

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def hello():
             </style>
         </head>
         <body>
-            <h1>Hello from AWS App Runner!</h1>
+            <h1>Hello from AWS Proton!</h1>
         </body>
     </html>
     '''
